01e6fd2e3207058b16dfa0e1f77b22b8149521c1
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2020-07-22 21:25:03 -0300

    Changes in some files

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,30 @@
-let rec;
-    if (!("webkitSpeechRecognition" in window)) {
-    	alert("disculpas, no puedes usar la API");
-    } else {
-    	rec = new webkitSpeechRecognition();
-    	rec.lang = "es-AR";
-    	rec.continuous = true;
-    	rec.interim = true;
-    	rec.addEventListener("result",iniciar);
-    }
-function iniciar(event){
-	for (let i = event.resultIndex; i < event.results.length; i++){
-         document.getElementById('texto').innerHTML = event.results[i][0].transcript;
-	}
-}
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const texto = document.getElementById("texto");
 
+let recognition = new webkitSpeechRecognition();
+recognition.lang = "es-ES";
+recognition.continuous = true;
+recognition.interimResults = false;
 
-rec.start();
+recognition.onresult = (event) => {
+  const results = event.results;
+  const frase = results[results.length - 1][0].transcript;
+  texto.value += frase;
+};
+
+recognition.onend = (event) => {
+  console.log("Deje de escuchar");
+};
+
+recognition.onerror = (event) => {
+  console.log;
+};
+
+start.addEventListener("click", () => {
+  recognition.start();
+});
+
+stop.addEventListener("click", () => {
+  recognition.abort();
+});
